69ecaddf07975c35f2edb92e67b73ec35ab9272c
claudianigro <claudia.nigro.cn@gmail.com>
2026-03-19 13:52:44 +0100

    Risoluzione problemi ai_service

--- backend/apps/ai_service/views.py
+++ backend/apps/ai_service/views.py
@@ -1,4 +1,3 @@
-import ai_service.serializers
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
@@ -17,7 +16,7 @@ from .duplicate_detector import check_duplicate
 def classify_ticket_view(request):
     serializer = TicketClassificationSerializer(data = request.data)
     if not serializer.is_valid():
-        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUESY)
+        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
     data = serializer.validated_data
     risultato_ai = classify_ticket(title=data['title'], description=data['description'])
     return Response(risultato_ai, status=status.HTTP_200_OK)
@@ -30,6 +29,7 @@ def generate_reply_view(request):
     data = serializer.validated_data
     risultato_ai = generate_reply( title = data['title'], description = data['description'], operator_notes = data.get('operator_notes', '')
     )
+    return Response({"generated_reply": risultato_ai}, status=status.HTTP_200_OK)
 
 @api_view(['POST'])
 def check_duplicate_view(request):
